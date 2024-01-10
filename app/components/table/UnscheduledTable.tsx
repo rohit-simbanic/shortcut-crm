04024c89db19e4ae8e4b16d23c608022c8ac33b6
rohit-simbanic <>
2024-01-10 19:31:06 +0530

    check responsiveness for shortcut app [sc-2172]

--- app/components/table/UnscheduledTable.tsx
+++ app/components/table/UnscheduledTable.tsx
@@ -55,7 +55,11 @@ const UnscheduledTable: FC<UnscheduledTableComponentProps> = ({
               isTeamManageOpen
                 ? ""
                 : "sticky z-20"
-            } top-0  bg-white dark:bg-[#161b26] max-[595px]:justify-center max-[595px]:items-center flex-wrap`}
+            } top-0  bg-white dark:bg-[#161b26] ${
+              isScheduledView
+                ? ""
+                : "max-[595px]:justify-center max-[595px]:items-center "
+            } flex-wrap`}
           >
             <div
               className={`flex items-center space-x-2 px-3 mr-8 rounded-md duration-500 ease-in-out py-2 cursor-pointer hover:bg-[#e4e8eb] hover:text-[#1f2532] dark:hover:bg-gray-950 dark:hover:text-[#635fc7] dark:text-white ${
